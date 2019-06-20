743c047e4b0dd6d255c106323c5075db89b1cfc6
choxmi <gwcsathsara@gmail.com>
2019-06-20 07:56:00 +0530

    login

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  win.loadFile('index.html')
+  win.loadFile('./login/login.html')
 
   // Open the DevTools.
   win.webContents.openDevTools()
